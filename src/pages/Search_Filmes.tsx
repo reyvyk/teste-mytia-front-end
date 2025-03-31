448656fc762c4e4f456fcd4656a6ccb30d6c95f7
Reyvyk Ferreira <reyvyk2@gmail.com>
2025-03-31 19:59:41 -0300

    refactoring pages to use custom hook service

--- src/pages/Search_Filmes.tsx
+++ src/pages/Search_Filmes.tsx
@@ -1,60 +1,16 @@
-import { useState, useEffect } from 'react'
 import { useSearchParams } from 'react-router-dom'
-import axios from 'axios'
+import useServiceAPI from '../hooks/useServiceAPI'
 
 import Filme_Card from '../components/Filme_Card/Filme_Card'
 
-const moviesURL = import.meta.env.VITE_API_SEARCH
-const apikey = import.meta.env.VITE_API_KEY
-
-interface Filmes {
-  id: number
-  title: string
-  overview: string
-  release_date: string
-  poster_path: string
-}
+const API_KEY = import.meta.env.VITE_API_KEY
+const API_SEARCH = import.meta.env.VITE_API_SEARCH
 
 const Search_Filmes = () => {
   const [searchParams] = useSearchParams()
+  const busca = searchParams.get('q')
 
-  const [searchResults, setSearchResults] = useState<Filmes[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-
-  const query = searchParams.get('q')
-
-  useEffect(() => {
-    const fetchData = async () => {
-      
-      setIsLoading(true)
-      setError(null)
-
-      try {
-        const response = await axios.get(
-          `${moviesURL}?${apikey}&query=${query}`
-        );
-        setSearchResults(response.data.results);
-      } catch (err) {
-        if (axios.isAxiosError(err)) {
-          setError(err.message)
-        } else if (err instanceof Error) {
-          setError(err.message)
-        } else {
-          setError('An unknown error occurred.');
-        }
-      } finally {
-        setIsLoading(false)
-      }
-    };
-
-    // Debounce the search to avoid making too many requests
-    const timeoutId = setTimeout(() => {
-      fetchData();
-    }, 500);
-
-    return () => clearTimeout(timeoutId); // Cleanup on unmount or searchTerm change
-  }, [query]);
+  const { data: searchResults, isLoading, error } = useServiceAPI<any>(`${API_SEARCH}${API_KEY}&query=${busca}`)
 
   return (
     <>
@@ -62,11 +18,11 @@ const Search_Filmes = () => {
       {error && <p>{error}</p>}
 
       <div className="flex flex-row flex-wrap justify-content-center">
-        {searchResults.map((filme) => (
-          <div key={filme.id}>
-            <Filme_Card filme={filme} />
-          </div>
-        ))}
+        {searchResults &&
+          searchResults.results.map((filme: any) => (
+            <Filme_Card key={filme.id} filme={filme} />
+          ))
+        }
       </div>
     </>
   );
